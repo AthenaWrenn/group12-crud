bcfb71af9c4640b2be9fa096726436430caead71
AthenaWrenn <162925464+AthenaWrenn@users.noreply.github.com>
2024-04-08 18:11:16 -0400

    Jaydens example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-ChartJS.register(ArcElement, Tooltip, Legend);
+import TimeOfYearQuery from './monthOfYearQuerys.js';
 
+import NumberOfTuples from './custom_components/num_of_tuples_comp.js';
 
 
 // https://react.dev/learn/conditional-rendering
@@ -22,32 +21,12 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {apiResponse: "" };
-        
-        // Used as an example for Chart.js
-        this.data = {
-          labels: ['Red', 'Blue', 'Yellow'],
-          datasets: [
-            {
-              label: '# of Votes',
-              data: [42, 314, 217],
-              backgroundColor: [
-                'rgba(255,  99, 132, 0.5)',
-                'rgba( 54, 162, 235, 0.5)',
-                'rgba(255, 206,  86, 0.5)',
-              ],
-              borderColor: [
-                'rgba(255,  99, 132, 1)',
-                'rgba( 54, 162, 235, 1)',
-                'rgba(255, 206,  86, 1)',
-              ],
-              borderWidth: 1,
-            },
-          ],
-        }
     }
+        
     
     // Used as an example to test connection to the API backend
     callAPI() {
+        // TODO: Change back to localhost
         fetch("http://localhost:8080/test")
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
@@ -63,22 +42,11 @@ class App extends Component {
       return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
             <TestAPIConnection
               apiRes={this.state.apiResponse} 
             />
-            <Doughnut data={this.data} />
+            <TimeOfYearQuery /> 
+            <NumberOfTuples /> 
           </header>
         </div>
       );
